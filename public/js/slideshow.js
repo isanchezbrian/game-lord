e4c010a56adc4580c630462d6668a1085829a115
RommelAquino <rommel.h.aquino@gmail.com>
2020-07-13 10:23:29 -0700

    got slideshow to work

--- public/js/slideshow.js
+++ public/js/slideshow.js
@@ -1,20 +1,20 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(x) {
-  showSlides(slideIndex += x);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function currentSlide(x) {
-  showSlides(slideIndex = x);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showSlides(x) {
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (x > slides.length) { slideIndex = 1 }
-  if (x < 1) { slideIndex = slides.length }
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
